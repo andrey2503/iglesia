bec2681d9ef7abfa0cff4c2ca887e1e04887dda7
andrey2503 <andreytorresvega@gmail.com>
2018-10-15 13:00:04 -0600

    agregar salidas

--- app/Http/Controllers/SalidaController.php
+++ app/Http/Controllers/SalidaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Salida;
 use Illuminate\Http\Request;
+use App\Logs;
 use App\Rubro;
 use App\CuentaBancaria;
 use App\CuentaPagar;
@@ -42,9 +43,168 @@ class SalidaController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $this->validate($request,[
+            'rubro'=>'required',
+            'descripcion'=>'required',
+            'moneda'=>'required',
+            'monto'=>'required',
+            'confMonto'=>'required',
+            'cuentaBancaria'=>'required',
+            'documento'=>'required',
+            'monto'=>'required|same:confMonto'
+          ]);
+  
+          $salidas = new Salida();
+          $salidas->descripcion = $request->descripcion;
+          $salidas->fk_rubro= $request->rubro;
+          $salidas->moneda= $request->moneda;
+          $salidas->monto=$request->monto;
+          $salidas->documento=$request->documento;
+  
+          if($salidas->save()){
+            $log= new Logs();
+            $log->fk_usuario= \Auth::user()->id;
+            $log->nombre_tabla="salidas";
+            $log->nombre_elemento= $salidas->id;
+            $log->accion="Agregar  Salida";
+            $log->fecha=date ('y-m-d H:i:s');
+            $log->save();
+            // return redirect()->back()->with('message','Salida '.$salidas->descripcion.' creada correctamente');
+          }
+  
+          if ($request->cuentaPagar1 !=0) {
+  
+          //  dd($request);
+            $this->addCuentaPagar($request);
+          }
+          if ($request->cuentaBancaria != 0) {
+              $this->addcuentaBancaria($request);
+          }
+          if ($request->cuentaCobrarD !=0) {
+  
+          //  dd($request);
+            $this->addCuentaCobrarDis($request);
+          }
+          if ($request->cuentaPagarD !=0) {
+  
+          //  dd($request);
+            $this->addCuentaPagarDis($request);
+          }
+          if ($request->cuentaPagarA !=0) {
+  
+          //  dd($request);
+            $this->addCuentaPagarAu($request);
+          }
+  
+          return redirect()->back()->with('message','Salida '.$request->descripcion.' creada correctamente');
+        }//fin metodo store
 
+
+    function addCuentaPagar($request) {
+        //dd($request);
+            $cuentasPagar = new CuentaPagar();
+            $cuentasPagar->nombre = $request->cuentaPagar;
+            $cuentasPagar->fk_rubro= $request->rubro;
+            $cuentasPagar->moneda= $request->moneda;
+            $cuentasPagar->monto=$request->monto;
+        
+            if($cuentasPagar->save()){
+              $log= new Logs();
+              $log->fk_usuario= \Auth::user()->id;
+              $log->nombre_tabla="cuenta_pagars";
+              $log->nombre_elemento= $cuentasPagar->id;
+              $log->accion="Agregar Cuenta por Pagar desde Entrada";
+              $log->fecha=date ('y-m-d H:i:s');
+              $log->save();
+        
+              //  return redirect()->back()->with('message','Cuenta por Cobrar '.$cuentasCobrar->nombre.' creada correctamente');
+          }
+        }//fin metodo addCuentaPagar
+
+
+        function addcuentaBancaria($request){
+            //verificar si cuenta vancaria es diferente de 0
+              $cuenta= CuentaBancaria::find($request->cuentaBancaria);
+                $montoAnterior=$cuenta->monto;
+              if ($cuenta->moneda == $request->moneda) {
+                $cuenta->monto=$request->monto + $montoAnterior;
+                if($cuenta->save()){
+                  $log= new Logs();
+                  $log->fk_usuario= \Auth::user()->id;
+                  $log->nombre_tabla="cuenta_bancarias";
+                  $log->nombre_elemento= $cuenta->id;
+                  $log->accion="Sumar ".$request->monto." al monto anterior: ".$montoAnterior;
+                  $log->fecha=date ('y-m-d H:i:s');
+                  $log->save();
+                  //  return redirect()->back()->with('message','Cuenta por Cobrar '.$cuentasCobrar->nombre.' creada correctamente');
+              }
+          
+                }
+          }//fin metodo addcuentaBancaria
+
+          //funcion para disminuir cuenta por cobrar
+        function addCuentaCobrarDis($request){
+            $cuentasCobrar=CuentaCobrar::find($request->cuentaCobrarD);
+            $montoAnterior=$cuentasCobrar->monto;
+            if ($cuentasCobrar->moneda == $request->moneda ) {
+            $cuentasCobrar->monto=$montoAnterior - $request->monto;
+            if($cuentasCobrar->save()){
+                $log= new Logs();
+                $log->fk_usuario= \Auth::user()->id;
+                $log->nombre_tabla="cuenta_cobrars";
+                $log->nombre_elemento= $cuentasCobrar->id;
+                $log->accion="Disminuir: ".$request->monto." al monto anterior: ".$montoAnterior;
+                $log->fecha=date ('y-m-d H:i:s');
+                $log->save();
+                //  return redirect()->back()->with('message','Cuenta por Cobrar '.$cuentasCobrar->nombre.' creada correctamente');
+            }
+        
+            }
+        
+        }//fin funcion disminuir cuenta por Cobrar
+
+        function addCuentaPagarDis($request){
+            $cuentasPagar=CuentaPagar::find($request->cuentaPagarD);
+            $montoAnterior=$cuentasPagar->monto;
+            if ($cuentasPagar->moneda == $request->moneda ) {
+              $cuentasPagar->monto=$montoAnterior - $request->monto;
+              if($cuentasPagar->save()){
+                $log= new Logs();
+                $log->fk_usuario= \Auth::user()->id;
+                $log->nombre_tabla="cuenta_pagars";
+                $log->nombre_elemento= $cuentasPagar->id;
+                $log->accion="Disminuir: ".$request->monto." al monto anterior: ".$montoAnterior;
+                $log->fecha=date ('y-m-d H:i:s');
+                $log->save();
+                //  return redirect()->back()->with('message','Cuenta por Cobrar '.$cuentasCobrar->nombre.' creada correctamente');
+            }
+          
+              }
+          
+          }//fin funcion disminuir cuenta por pagar
+
+          //funcion para aumentar cuenta por pagar
+        function addCuentaPagarAu($request){
+            $cuentasPagar=CuentaPagar::find($request->cuentaPagarA);
+            $montoAnterior=$cuentasPagar->monto;
+            if ($cuentasPagar->moneda == $request->moneda ) {
+            $cuentasPagar->monto=$montoAnterior + $request->monto;
+            if($cuentasPagar->save()){
+                $log= new Logs();
+                $log->fk_usuario= \Auth::user()->id;
+                $log->nombre_tabla="cuenta_pagars";
+                $log->nombre_elemento= $cuentasPagar->id;
+                $log->accion="Aumentar: ".$request->monto." al monto anterior: ".$montoAnterior;
+                $log->fecha=date ('y-m-d H:i:s');
+                $log->save();
+                //  return redirect()->back()->with('message','Cuenta por Cobrar '.$cuentasCobrar->nombre.' creada correctamente');
+            }
+        
+            }
+        
+        }//fin funcion aumentar cuenta por pagar
+
+        //fin del update
     /**
      * Display the specified resource.
      *
