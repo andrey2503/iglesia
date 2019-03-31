a2943952c170d35fc1f37118fd4a1e0c88954684
Andrey Torres Vega <andreytorresvega@gmail.com>
2019-03-31 08:52:28 -0600

    arreglo de fecha

--- app/Http/Controllers/CuentaBancariaController.php
+++ app/Http/Controllers/CuentaBancariaController.php
@@ -210,73 +210,39 @@ if($request->tipoReporte != 0){
   return view('administrador.reportesCuentasBancarias')->with([
       'cuentas'=>$cuentas,
       'tipoReporte'=>$request->tipoReporte,
-      'fechaInicio'=>'',
-      'fechaFinal'=>'',
+      'fechaInicio'=>$fechaInicio,
+      'fechaFinal'=>$fechaFinal,
       'titulo'=>$request->titulo,
       'mov_salida'=>$mov_salida,
       'mov_entrada'=>$mov_entrada
       ]);
 
 }
-// if($request->tipoReporte == 1){
-// // dd($request);
-// $this->validate($request,[
-//     'fechaInicio'=>'required|date',
-//     'fechaFinal'=>'required|date',
-//     ]);
-//   $fechaInicio=Carbon::parse($request->fechaInicio)->format('Y-m-d');
-//   $fechaFinal=Carbon::parse($request->fechaFinal)->format('Y-m-d');
-//   $cuentas= CuentaBancaria::where('fechaRegistro','>=',$fechaInicio)->where('fechaRegistro','<=',$fechaFinal)->get();
-//   // dd($cuentas);
-//   return view('administrador.reportesCuentasBancarias')->with(['cuentas'=>$cuentas,'tipoReporte'=>$request->tipoReporte,'fechaInicio'=>$fechaInicio,'fechaFinal'=>$fechaFinal,'titulo'=>$request->titulo]);
-// }
-
-// if($request->tipoReporte == 3){
-//     // dd($request);
-//     // $this->validate($request,[
-//     //     'fechaInicio'=>'required|date',
-//     //     'fechaFinal'=>'required|date',
-//     //     ]);
-//     //   $fechaInicio=Carbon::parse($request->fechaInicio)->format('Y-m-d');
-//     //   $fechaFinal=Carbon::parse($request->fechaFinal)->format('Y-m-d');
-//       $cuentas= CuentaBancaria::where('tipo','=','Corriente')->get();
-//       // dd($cuentas);
-//       return view('administrador.reportesCuentasBancarias')->with(['cuentas'=>$cuentas,'tipoReporte'=>$request->tipoReporte,'fechaInicio'=>'','fechaFinal'=>'','titulo'=>$request->titulo]);
-//     }
-
-    // if($request->tipoReporte == 4){
-    //     // dd($request);
-    //     // $this->validate($request,[
-    //     //     'fechaInicio'=>'required|date',
-    //     //     'fechaFinal'=>'required|date',
-    //     //     ]);
-    //     //   $fechaInicio=Carbon::parse($request->fechaInicio)->format('Y-m-d');
-    //     //   $fechaFinal=Carbon::parse($request->fechaFinal)->format('Y-m-d');
-    //       $cuentas= CuentaBancaria::where('tipo','=','Ahorros')->get();
-    //       // dd($cuentas);
-    //       return view('administrador.reportesCuentasBancarias')->with(['cuentas'=>$cuentas,'tipoReporte'=>$request->tipoReporte,'fechaInicio'=>'','fechaFinal'=>'','titulo'=>$request->titulo]);
-    // }
 
 }// fin de reportes
 
 public function reporte(Request $request){
+// dd($request);
+if($request->tipoReporte == 0){
 
-  if($request->tipoReporte == 2){
+}
+//   if($request->tipoReporte == 2){
 
-      $cuentas= CuentaBancaria::all();
-      $view= view('reportes.pdfReporteCuentaBancaria')->with(['cuentas'=>$cuentas,'tipoReporte'=>$request->tipoReporte,'fechaInicio'=>'','fechaFinal'=>'']);
-      unset($pdf);
-      $pdf=\App::make('dompdf.wrapper');
-      $pdf->loadhtml($view);
-      return $pdf->stream('document.pdf');
-  //  dd($cuentas);
+//       $cuentas= CuentaBancaria::all();
+//       $view= view('reportes.pdfReporteCuentaBancaria')->with(['cuentas'=>$cuentas,'tipoReporte'=>$request->tipoReporte,'fechaInicio'=>'','fechaFinal'=>'']);
+//       unset($pdf);
+//       $pdf=\App::make('dompdf.wrapper');
+//       $pdf->loadhtml($view);
+//       return $pdf->stream('document.pdf');
+//   //  dd($cuentas);
 
-    // return view('reportes.pdfReporteCuentaBancaria')->with(['cuentas'=>$cuentas,'tipoReporte'=>$request->tipoReporte,'fechaInicio'=>'','fechaFinal'=>'']);
-  }
-  if($request->tipoReporte == 1){
+//     // return view('reportes.pdfReporteCuentaBancaria')->with(['cuentas'=>$cuentas,'tipoReporte'=>$request->tipoReporte,'fechaInicio'=>'','fechaFinal'=>'']);
+//   }
+  if($request->tipoReporte != 0){
     $this->validate($request,[
         'fechaInicio'=>'required',
         'fechaFinal'=>'required',
+        'tipoReporte'=>'required'
         ]);
         $fechaInicio=Carbon::parse($request->fechaInicio)->format('Y-m-d');
         $fechaFinal=Carbon::parse($request->fechaFinal)->format('Y-m-d');
